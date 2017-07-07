13197268631ce4564dea5ac108ec2b8c4ce71e04
Carmen Salas <clivingrooms@aol.com>
2017-07-07 19:36:59 +0000

    my website using ntml css and javascript

--- script.js
+++ script.js
@@ -3,4 +3,35 @@
 $( document ).ready(function() {
 
 
+    $("#button").click(function(){
+        var inputValue = $("#inputValue").val();
+        var answer = "senior year";
+        var answer2 = "senior";
+        var answer3 = "12";
+        
+        
+        if(inputValue === answer) {
+            $("h3").append("Correct!");
+        }
+        
+        else if (inputValue === answer2){
+            $("h3").html("Correct!");
+        }
+        
+        else if (inputValue === answer3) {
+            $("h3").html("Correct!");
+        }
+        
+        });
+
+
+
+
+
+
+
+
+
+
+
 });
